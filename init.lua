674afa2b2d45b10f6f27f4fde5b31529d0d866f8
spring <wenchuan.lin@abagile.com>
2022-05-14 11:40:59 +0800

    Migrate keymaps

--- init.lua
+++ init.lua
@@ -1,9 +1,65 @@
 local indent, width = 2, 80
-vim.g.mapleader = ','
-
 -- Lua modules are found inside a lua/ folder in your 'runtimepath' (for most users, this will mean ~/.config/nvim/lua
 require('wc.plugins')
 require('wc.options')
+
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap leader key
+vim.g.mapleader = ','
+-- vim.g.maplocalleader = " "
+
+-- Modes
+--   normlal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+--  discard direction keys
+keymap("n", "<up>", "<nop>", opts)
+keymap("n", "<down>", "<nop>", opts)
+keymap("n", "<left>", "<nop>", opts)
+keymap("n", "<right>", "<nop>", opts)
+
+keymap("i", "<up>", "<nop>", opts)
+keymap("i", "<down>", "<nop>", opts)
+keymap("i", "<left>", "<nop>", opts)
+keymap("i", "<right>", "<nop>", opts)
+
+-- sometimes need, to repeat latest f, t, F or T in opposite direction
+keymap("", "\\", ",", opts)
+
+-- " Helps when I want to delete something without clobbering my unnamed register.
+keymap("n", "s", '"_d', opts)
+keymap("n", "ss", '"_dd', opts)
+
+-- " navigating
+keymap("n", "H", '^', opts)
+keymap("n", "L", '$', opts)
+keymap("n", "j", 'gj', term_opts)
+keymap("n", "k", 'gk', term_opts)
+keymap("n", ",gv", 'V`', opts)
+
+-- " Keeping it centered
+keymap("n", "n", 'nzzzv', opts)
+keymap("n", "N", 'Nzzzv', opts)
+keymap("n", "J", 'mzJ`z', opts)
+
+keymap("n", "<C-n>", "<C-i>", opts)
+
+keymap("n", "Y", "yg_", opts)
+
+-- " moving text
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
+-- " Don't copy the contents of an overwritten selection.
+keymap("v", "p", '"_dP', opts)
 
 --  Vim Tmux Navigator
 vim.g['tmux_navigator_disable_when_zoomed'] = 1
