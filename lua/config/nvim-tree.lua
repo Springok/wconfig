97307f16793526398c381b8d5b86ec380681ef91
spring <wenchuan.lin@abagile.com>
2022-05-21 19:59:42 +0800

    Update nvim tree settings

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -105,6 +105,7 @@ nvim_tree.setup {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "x", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
+        { key = "m", cb = tree_cb "rename" },
       },
     },
     number = false,
