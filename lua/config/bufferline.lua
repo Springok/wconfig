544b58d6b4f2584cef88d891917540c9439bc575
spring <wenchuan.lin@abagile.com>
2022-06-12 16:51:54 +0800

    Consolidate configs
    Relocate the keymaps, config back to init.lua, if it is not so
    complicated, and remove the files.

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -3,6 +3,36 @@ if not status_ok then
   return
 end
 
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+    globalstatus = true,
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
+
 bufferline.setup {
   options = {
     mode = "buffers",
--- lua/config/conjure.lua
+++
@@ -1,5 +0,0 @@
-vim.g['conjure#log#hud#width'] = 0.7
-vim.g['conjure#log#hud#height'] = 0.7
-vim.g['conjure#log#hud#anchor'] = "SE"
-vim.g['conjure#highlight#enable'] = 'true'
-vim.g['conjure#log#botright'] = 'true'
