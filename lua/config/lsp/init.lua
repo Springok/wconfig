6ca53849b59bba62b5661f47354102ea240484c4
spring <wenchuan.lin@abagile.com>
2022-06-12 15:25:26 +0800

    Update key mappings about LSP config
    Copied from godwin dada, hahaha.
    
    Good idea about localleader when the command is related to lsp.

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -56,26 +56,25 @@ local function lsp_highlight_document(client)
 end
 
 local function lsp_keymaps(bufnr)
-  local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
-  vim.api.nvim_buf_set_keymap(
-    bufnr,
-    "n",
-    "gl",
-    '<cmd>lua vim.diagnostic.open_float()<CR>',
-    opts
-  )
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>dl", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+  local keymap = vim.keymap.set
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+
+  keymap("n", "[d", vim.diagnostic.goto_prev, opts)
+  keymap("n", "]d", vim.diagnostic.goto_next, opts)
+  keymap("n", "<leader>dl", vim.diagnostic.setloclist, opts)
+
+  keymap("n", "K", vim.lsp.buf.hover, opts)
+  keymap("n", "gD", vim.lsp.buf.declaration, opts)
+  keymap("n", "gd", vim.lsp.buf.definition, opts)
+  keymap("n", "<localleader>li", vim.lsp.buf.implementation, opts)
+  keymap("n", "<localleader>lr", vim.lsp.buf.references, opts)
+  keymap("n", "<localleader>lt", vim.lsp.buf.type_definition, opts)
+  keymap("n", "<localleader>lh", vim.lsp.buf.signature_help, opts)
+  keymap("n", "<localleader>ln", vim.lsp.buf.rename, opts)
+  keymap("n", "<localleader>la", vim.lsp.buf.code_action, opts)
+  keymap("v", "<localleader>la", vim.lsp.buf.range_code_action, opts)
+  -- keymap("n", "<localleader>lf", vim.lsp.buf.formatting, opts)
+
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
