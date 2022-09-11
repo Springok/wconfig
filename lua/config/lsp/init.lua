6d91710a7e011fc4ce3da0e01ad7633e684838ca
spring <wenchuan.lin@abagile.com>
2022-09-11 13:21:00 +0800

    Disable Ruby ISP server

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -85,7 +85,8 @@ end
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-local servers = { "jsonls", "sumneko_lua", "solargraph", "clojure_lsp" }
+-- local servers = { "jsonls", "sumneko_lua", "solargraph", "clojure_lsp" }
+local servers = { "jsonls", "sumneko_lua", "clojure_lsp" }
 
 require("nvim-lsp-installer").setup {
   ensure_installed = servers
