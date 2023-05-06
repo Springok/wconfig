4015ff560a312ddfc78ad411de6c9d58d51952d5
Wenchuan Lin <wenchuan.lin@abagile.com>
2023-05-06 10:48:03 +0800

    Change theme in Vim

--- init.lua
+++ init.lua
@@ -18,12 +18,13 @@ require "config.bufferline"
 -- ================
 --      Theme
 -- ================
-vim.g.onedark_config = { style = "warmer" }
-vim.cmd [[colorscheme onedark]]
+-- vim.g.onedark_config = { style = "warmer" }
+-- vim.cmd [[colorscheme onedark]]
 -- vim.g.vscode_style = "dark"
 -- vim.cmd [[colorscheme vscode]]
 -- vim.cmd [[colorscheme tokyonight]]
--- vim.cmd [[colorscheme material]]
+vim.g.material_style = "darker"
+vim.cmd [[colorscheme material]]
 
 -- =================
 --      Plugins
